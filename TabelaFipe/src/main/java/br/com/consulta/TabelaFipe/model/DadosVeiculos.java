971331412083502f4ba5bc76184b58ed38446c6d
Abner <abner.rmachado@gmail.com>
2023-12-03 00:10:45 -0300

    delete

--- TabelaFipe/src/main/java/br/com/consulta/TabelaFipe/model/DadosVeiculos.java
+++
@@ -1,4 +0,0 @@
-package br.com.consulta.TabelaFipe.model;
-
-public record DadosVeiculos(String codigo, String nome) {
-}
--- TabelaFipe/src/main/java/br/com/consulta/TabelaFipe/service/IConverteDados.java
+++
@@ -1,8 +0,0 @@
-package br.com.consulta.TabelaFipe.service;
-
-import java.util.List;
-
-public interface IConverteDados {
-    <T> T obterDados(String json, Class<T> classe);
-    <T> List<T> obterLista(String json, Class<T> classe);
-}
